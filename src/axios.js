8cbd0c8a874ebe4ccc41955a6449a9d02596bff7
Fachi-r <farairubvuta@gmail.com>
2022-07-08 06:00:38 +0200

    rewrote http apis to https

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base URL to make requests to the movie database
 const instance = axios.create({
-   baseURL: "http://api.themoviedb.org/3",
+   baseURL: "https://api.themoviedb.org/3",
 });
 
 instance.get();
